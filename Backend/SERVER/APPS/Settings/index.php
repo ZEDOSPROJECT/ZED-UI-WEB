5a8499201347ebbf3acc62340153c1468948f081
Alexandre Nunes <alexandre.nunes@flipkick.cloud>
2018-10-19 16:23:19 +0100

    New Settings page

--- Backend/SERVER/APPS/Settings/index.php
+++ Backend/SERVER/APPS/Settings/index.php
@@ -5,7 +5,16 @@
 		<TITLE>Settings</TITLE>
 	</HEAD>
 	<BODY>
-	
+		<form>
+			<h2>Personalization:</h2>
+			<p>Wallpaper URL: <input type="text" id="setting_wallpaperURL" value="wallpaper.jpg"/></p>
+			<p>Bing Wallpaper: <input type="checkbox" id="setting_bingWallpaper"/></p>
+			<p>Background color: <input type="color" id="setting_wallpaperColor" value="#004e98"></p>
+			<br>
+			<hr>
+			<button type="reset">DEFAULTS</button>
+			<button>SAVE</button>
+		</form>
 	</BODY>
 
 </html>
