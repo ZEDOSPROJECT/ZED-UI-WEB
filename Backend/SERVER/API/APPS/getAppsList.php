36cf1c37513fc4bfe14640855cdfbcde18eaa2c9
ZEDOSPROJECT <zedos.project@gmail.com>
2018-11-21 23:08:21 +0000

    Changes in filter

--- Backend/SERVER/API/APPS/getAppsList.php
+++ Backend/SERVER/API/APPS/getAppsList.php
@@ -3,18 +3,20 @@
     $FINAL_LIST=[];
     $UA=$_SERVER['HTTP_USER_AGENT'];
     foreach (scandir($_SERVER["DOCUMENT_ROOT"]."/APPS") as $app) {
-        if($UA!="MOBILE" or $UA!="DESKTOP"){
-            $content=file_get_contents($_SERVER["DOCUMENT_ROOT"]."/APPS/".$app."/PLATFORMS");
-            if (strpos($content, "WEB") !== false) {
-                array_push($FINAL_LIST,$app);
+        if($app != ".." and $app != "."){
+            if($UA=="DESKTOP"){
+                $content=file_get_contents($_SERVER["DOCUMENT_ROOT"]."/APPS/".$app."/PLATFORMS");
+                if (strpos($content, "DESTOP") !== false) {
+                    array_push($FINAL_LIST,$app);
+                }
             }
-        }
-        else
-        {
-            $content=file_get_contents($_SERVER["DOCUMENT_ROOT"]."/APPS/".$app."/PLATFORMS");
-            if (strpos($content, $UA) !== false) {
-                array_push($FINAL_LIST,$app);
-            }
+            else
+            {
+                $content=file_get_contents($_SERVER["DOCUMENT_ROOT"]."/APPS/".$app."/PLATFORMS");
+                if (strpos($content, "WEB") !== false) {
+                    array_push($FINAL_LIST,$app);
+                }
+            } 
         } 
     }
     echo json_encode($FINAL_LIST);
