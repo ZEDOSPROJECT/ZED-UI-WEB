82a0e5e8fff9f9e1cf3815f7be9170226bcd0397
ZEDOSPROJECT <zedos.project@gmail.com>
2018-12-12 22:02:00 +0000

    Update photoshop url

--- Backend/SERVER/APPS/Photoshop/index.php
+++ Backend/SERVER/APPS/Photoshop/index.php
@@ -1,3 +1,3 @@
 <script>
-  window.location = "http://www.photoshoponline.net.br/editor?utm_source=home-photoshop-online&utm_medium=botao-na-home&utm_campaign=aviso";
+  window.location = "https://www.photopea.com/";
 </script>
