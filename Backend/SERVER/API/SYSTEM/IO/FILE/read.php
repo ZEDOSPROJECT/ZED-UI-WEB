06335fa4d715ebde9559db503279e6b2f9ec463a
ZEDOSPROJECT <zedos.project@gmail.com>
2019-01-02 22:31:21 +0000

    Improve Read File in API

--- Backend/SERVER/API/SYSTEM/IO/FILE/read.php
+++ Backend/SERVER/API/SYSTEM/IO/FILE/read.php
@@ -1,5 +1,19 @@
 <?php
 	header("Access-Control-Allow-Origin: *");
 	$PATH=$_GET['path'];
-	echo file_get_contents($PATH);
+
+	$file = $_GET['path'];
+	
+	if(!file_exists($file)){ // file does not exist
+		die();
+	} else {
+		header("Cache-Control: public");
+		header("Content-Description: File Transfer");
+		header("Content-Disposition: attachment; filename=".basename($_GET['path']));
+		header("Content-Type: ".mime_content_type($_GET['path']));
+		header("Content-Transfer-Encoding: binary");
+	
+		// read the file from disk
+		readfile($file);
+	}
 ?>
