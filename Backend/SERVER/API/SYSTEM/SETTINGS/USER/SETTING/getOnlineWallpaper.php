9f00f80067e38f02fd5f5ef6994fa740e54ad1ab
ZEDOSPROJECT <zedos.project@gmail.com>
2019-02-09 23:33:38 +0000

    Fix Bing Wallpaper gradient effect

--- Backend/SERVER/API/SYSTEM/SETTINGS/USER/SETTING/getOnlineWallpaper.php
+++ Backend/SERVER/API/SYSTEM/SETTINGS/USER/SETTING/getOnlineWallpaper.php
@@ -5,5 +5,5 @@
     $settings['setting_wallpaperURL']="Online.jpg";
     $newJsonString = json_encode($settings);
     file_put_contents('../SETTINGS.json', $newJsonString);
-    file_put_contents( __DIR__."/Wallpapers/Online.jpg", fopen($url, 'r'));
+    file_put_contents( $_SERVER['DOCUMENT_ROOT']."/Wallpapers/Online.jpg", fopen($url, 'r'));
 ?>
