0c840f4bbf87a99106b76e8de1454daad06891e5
ZEDOSPROJECT <zedos.project@gmail.com>
2019-02-10 23:12:32 +0000

    Improve set as wallpaper

--- Backend/SERVER/API/SYSTEM/SETTINGS/USER/SETTING/setWallpaper.php
+++ Backend/SERVER/API/SYSTEM/SETTINGS/USER/SETTING/setWallpaper.php
@@ -1,8 +1,16 @@
 <?php
     header("Access-Control-Allow-Origin: *");
-    copy($_GET['path'] , "../../../../../Wallpapers/".basename($_GET['path']));
+    $path_info = pathinfo($_GET['path']);
+    $finalFile = uniqid()."_EXTERNAL.".$path_info['extension'];
+   
+    foreach (scandir($_SERVER['DOCUMENT_ROOT']."/Wallpapers/") as $filename) {
+        if (strpos($filename, '_EXTERNAL.') !== false) {
+            unlink($_SERVER['DOCUMENT_ROOT']."/Wallpapers/".$filename);
+        }
+    }
+    copy($_GET['path'] , $_SERVER['DOCUMENT_ROOT']."/Wallpapers/".$finalFile);
     $settings = json_decode(file_get_contents('../SETTINGS.json'),true);
-    $settings['setting_wallpaperURL']=basename($_GET['path']);
+    $settings['setting_wallpaperURL']=$finalFile;
     $newJsonString = json_encode($settings);
     file_put_contents('../SETTINGS.json', $newJsonString);
 ?>
