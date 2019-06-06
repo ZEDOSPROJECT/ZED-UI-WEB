1648c42c17e47fd080ef43a97308e9679cf721de
Alexandre Nunes <alexandrenunes1331@gmail.com>
2019-06-06 19:36:51 +0100

    Add Get System Info in API

---
+++ Backend/SERVER/API/SYSTEM/getInfo.php
@@ -0,0 +1,44 @@
+<?php
+    header("Access-Control-Allow-Origin: *");
+    $data['OS'] = "ZED OS";
+    $data['VERSION'] = "2019.0";
+    $data['LINUX KERNEL'] = php_uname("r");
+
+    $cpuinfo = file('/proc/cpuinfo');
+    
+
+    $data['CPU'] = explode(":",$cpuinfo[4])[1];
+    $data['RAM'] = round(intval(getSystemMemInfo()["MemTotal"])/1024/1024)." GB";
+    $data['STORAGE'] = ConvertBytes(disk_total_space("/"));
+    echo json_encode($data);
+
+    function getSystemMemInfo() 
+    {       
+        $data = explode("\n", file_get_contents("/proc/meminfo"));
+        $meminfo = array();
+        foreach ($data as $line) {
+            list($key, $val) = explode(":", $line);
+            $meminfo[$key] = trim($val);
+        }
+        return $meminfo;
+    }  
+    
+    function ConvertBytes($number)
+    {
+        $len = strlen($number);
+        if($len < 4)
+        {
+            return sprintf("%d b", $number);
+        }
+        if($len >= 4 && $len <=6)
+        {
+            return sprintf("%0.2f Kb", $number/1024);
+        }
+        if($len >= 7 && $len <=9)
+        {
+            return sprintf("%0.2f Mb", $number/1024/1024);
+        }
+        return sprintf("%0.2f Gb", $number/1024/1024/1024);                           
+    }
+
+?>
