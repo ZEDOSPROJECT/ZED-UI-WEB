539cea06e033c64c9ef1cd14691365a2fe4ec6c6
Alexandre Nunes <alexandrenunes1331@gmail.com>
2019-09-07 12:56:02 +0100

    [FIx] Fix set audio in some audio conditions

--- Backend/SERVER/API/SYSTEM/SETTINGS/setAudioVolume.php
+++ Backend/SERVER/API/SYSTEM/SETTINGS/setAudioVolume.php
@@ -2,4 +2,5 @@ amixer set Master 50%
 <?php
     header("Access-Control-Allow-Origin: *");
     shell_exec("amixer -D pulse set Master ".$_GET['volume']."%");
+    shell_exec("amixer set Master ".$_GET['volume']."%");
 ?>
