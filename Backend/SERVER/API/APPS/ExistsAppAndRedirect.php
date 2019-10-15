7d7868509f6f4b4e6090f335aca2533c14a9ee4f
Alexandre Nunes <alexandrenunes1331@gmail.com>
2019-10-15 20:22:48 +0100

    [ADD] Add check if app is installed and redirect to store

---
+++ Backend/SERVER/API/APPS/ExistsAppAndRedirect.php
@@ -0,0 +1,29 @@
+<?php
+
+    header("Access-Control-Allow-Origin: *");
+    header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+    header("Cache-Control: post-check=0, pre-check=0", false);
+    header("Pragma: no-cache");
+    if(folder_exist("../../APPS/".$_GET['appName'])){
+        header("Location: http://zed-os.sourceforge.net/store/?APPNAME=".$_GET['appName']."&isInstalled=1");
+        die();
+    }else{
+        header("Location: http://zed-os.sourceforge.net/store/?APPNAME=".$_GET['appName']);
+        die();
+    }
+    function folder_exist($folder)
+    {
+        // Get canonicalized absolute pathname
+        $path = realpath($folder);
+
+        // If it exist, check if it's a directory
+        if($path !== false AND is_dir($path))
+        {
+            // Return canonicalized absolute pathname
+            return $path;
+        }
+
+        // Path/folder does not exist
+        return false;
+    }
+?>
