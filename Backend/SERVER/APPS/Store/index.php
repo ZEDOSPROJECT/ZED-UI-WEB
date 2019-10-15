7bc02a00a978babea1c1798dd0fc683687a01e4a
Alexandre Nunes <alexandrenunes1331@gmail.com>
2019-10-15 22:43:32 +0100

    [ADD] Home page of store

--- Backend/SERVER/APPS/Store/index.php
+++ Backend/SERVER/APPS/Store/index.php
@@ -1,3 +1,23 @@
-<script>
-  window.location = "http://127.0.0.1:3031/API/APPS/ExistsAppAndRedirect.php?appName=TuneIn";
-</script>
+<HTML>
+  <HEAD>
+    <TITLE>Store</TITLE>
+    <script>
+      function openApp(app){
+        window.location.href = "http://localhost:3031/API/APPS/ExistsAppAndRedirect.php?appName="+app;
+      }
+    </script>
+  </HEAD>
+  <BODY>
+    <?php
+      $json = json_decode(file_get_contents('http://zed-os.sourceforge.net/store/getAppList.php'),false);
+      for ($i=0; $i < sizeof($json->files); $i++) { 
+        echo('<div class="appCard" onClick="openApp(\''.$json->files[$i]->file.'\')">');
+        echo('<img class="appIcon" src="http://zed-os.sourceforge.net/store/DIRECTORY/'.$json->files[$i]->file.'/favicon.png"/>');
+        echo('<div class="appTitle"><h2>'.$json->files[$i]->file.'</h2></div>');
+        echo("</div>");
+      } 
+    ?>
+  </BODY>
+</HTML>
+
+<link rel="stylesheet" type="text/css" href="css/style.css" />
