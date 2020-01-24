4f276345b6d820e48d95c7bee3b8a84fd1268229
Alexandre Nunes <alexandrenunes1331@gmail.com>
2020-01-24 22:04:02 +0000

    [ADD] Show IP's adresses in control panel

--- Backend/SERVER/APPS/Settings/source/src/Screens/Network/Network.js
+++ Backend/SERVER/APPS/Settings/source/src/Screens/Network/Network.js
@@ -1,9 +1,36 @@
 import React from 'react';
 
 export default class Network extends React.Component {
+    constructor(props){
+        super(props);
+        this.state={
+            localIP:"0.0.0.0",
+            ExternalIP:"0.0.0.0"
+        }
+
+
+        fetch("http://" +
+        window.location.hostname +
+        ":3031/API/SYSTEM/NETWORK/getExternalIP.php")
+        .then(response => response.text())
+        .then(text => {
+            this.setState({ ExternalIP: text });
+        });
+
+        fetch("http://" +
+        window.location.hostname +
+        ":3031/API/SYSTEM/NETWORK/getPrivateIP.php")
+        .then(response => response.text())
+        .then(text => {
+            this.setState({ localIP: text });
+        });
+    }
     render(){
         return(
-            <div>Not implemented</div>
+            <div>
+                <div>My External IP Adress: {this.state.ExternalIP}</div>
+                <div>My Local IP Adress: {this.state.localIP}</div>
+            </div>
         )
     }
 }
