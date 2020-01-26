0af85124e72ebfdefc47f4cf9a8d0f173187ad86
Alexandre Nunes <alexandrenunes1331@gmail.com>
2020-01-26 20:03:12 +0000

    [CHANGE] Change to ifconfig.me to get external ip adress

--- Backend/SERVER/API/SYSTEM/NETWORK/getExternalIP.php
+++ Backend/SERVER/API/SYSTEM/NETWORK/getExternalIP.php
@@ -1,5 +1,5 @@
 <?php
     header("Access-Control-Allow-Origin: *");
-    $realIP = file_get_contents("http://ipecho.net/plain");
+    $realIP = file_get_contents("http://ifconfig.me/ip");
 	echo $realIP;
 ?>
