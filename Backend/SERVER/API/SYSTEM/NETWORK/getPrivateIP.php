abef533355000b4bd31339de53b5e3ad9f33f7c6
Alexandre Nunes <alexandrenunes1331@gmail.com>
2020-01-27 21:01:37 +0000

    [FIX] Fix getting local ip adress

--- Backend/SERVER/API/SYSTEM/NETWORK/getPrivateIP.php
+++ Backend/SERVER/API/SYSTEM/NETWORK/getPrivateIP.php
@@ -1,4 +1,3 @@
 <?php
     header("Access-Control-Allow-Origin: *");
-    echo getHostByName(getHostName());
-?>
+    echo exec("ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'");
