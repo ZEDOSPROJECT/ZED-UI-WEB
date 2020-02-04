3e2f7d159dbcd9184b226f6ffbf3cd999d58a36f
Alexandre Nunes <alexandrenunes1331@gmail.com>
2020-02-04 20:59:05 +0000

    [ADD/IMPROVE] Add Search engine and improve UI

--- Backend/SERVER/APPS/Store/index.php
+++ Backend/SERVER/APPS/Store/index.php
@@ -1,11 +1,27 @@
 <HTML>
   <HEAD>
     <script>
+      <?php
+        echo 'let currentType="'.$_GET['type'].'";';
+      ?>
+
+      let timerKeys=null;
       function openApp(app){
         window.location.href = "http://localhost:3031/API/APPS/ExistsAppAndRedirect.php?appName="+app;
       }
       function loadType(type){
         window.location.href = "index.php?type="+type;
+      }
+      function search(ele) {
+          if(timerKeys !== null){
+            clearTimeout(timerKeys);
+          }
+          timerKeys=setTimeout(function(){
+            window.location.href = "index.php?type="+currentType+"&query="+ele.value;
+          }, 30);
+          if(event.key === 'Enter') {
+            window.location.href = "index.php?type="+currentType+"&query="+ele.value;
+          }
       }
     </script>
     <title>Store</title>
@@ -18,41 +34,47 @@
           }else{
             $type="all";
           }
-          if($type=="game"){
-            echo('<div class="typeIcon selected"><img class="typeImg" width="24" height="24" src="imgs/games.png"/><div class="typelbl">Games</div></div>');
+          if(isset($_GET['query'])){
+            $query=$_GET['query'];
           }else{
-            echo('<div onclick="loadType(\'game\')" class="typeIcon"><img class="typeImg" width="24" height="24" src="imgs/games.png"/><div class="typelbl">Games</div></div>');
+            $query="";
+          }
+          if($type=="game"){
+            echo('<div class="typeIcon selected"><img class="typeImg" width="24" height="24" src="imgs/games.png"/></div>');
+          }else{
+            echo('<div onclick="loadType(\'game\')" class="typeIcon"><img class="typeImg" width="24" height="24" src="imgs/games.png"/></div>');
           }
           if($type=="music"){
-            echo('<div class="typeIcon selected"><img class="typeImg" width="24" height="24" src="imgs/music.png"/><div class="typelbl">Music</div></div>');
+            echo('<div class="typeIcon selected"><img class="typeImg" width="24" height="24" src="imgs/music.png"/></div>');
           }else{
-            echo('<div onclick="loadType(\'music\')" class="typeIcon"><img class="typeImg" width="24" height="24" src="imgs/music.png"/><div class="typelbl">Music</div></div>');
+            echo('<div onclick="loadType(\'music\')" class="typeIcon"><img class="typeImg" width="24" height="24" src="imgs/music.png"/></div>');
           }
           if($type=="video"){
-            echo('<div class="typeIcon selected"><img class="typeImg" width="24" height="24" src="imgs/video.png"/><div class="typelbl">Video</div></div>');
+            echo('<div class="typeIcon selected"><img class="typeImg" width="24" height="24" src="imgs/video.png"/></div>');
           }else{
-            echo('<div onclick="loadType(\'video\')" class="typeIcon"><img class="typeImg" width="24" height="24" src="imgs/video.png"/><div class="typelbl">Video</div></div>');
+            echo('<div onclick="loadType(\'video\')" class="typeIcon"><img class="typeImg" width="24" height="24" src="imgs/video.png"/></div>');
           }
           if($type=="picture"){
-            echo('<div class="typeIcon selected"><img class="typeImg" width="24" height="24" src="imgs/picture.png"/><div class="typelbl">Picture</div></div>');
+            echo('<div class="typeIcon selected"><img class="typeImg" width="24" height="24" src="imgs/picture.png"/></div>');
           }else{
-            echo('<div onclick="loadType(\'picture\')" class="typeIcon"><img class="typeImg" width="24" height="24" src="imgs/picture.png"/><div class="typelbl">Picture</div></div>');
+            echo('<div onclick="loadType(\'picture\')" class="typeIcon"><img class="typeImg" width="24" height="24" src="imgs/picture.png"/></div>');
           }
           if($type=="internet"){
-            echo('<div class="typeIcon selected"><img class="typeImg" width="24" height="24" src="imgs/internet.png"/><div class="typelbl">Internet</div></div>');
+            echo('<div class="typeIcon selected"><img class="typeImg" width="24" height="24" src="imgs/internet.png"/></div>');
           }else{
-            echo('<div onclick="loadType(\'internet\')" class="typeIcon"><img class="typeImg" width="24" height="24" src="imgs/internet.png"/><div class="typelbl">Internet</div></div>');
+            echo('<div onclick="loadType(\'internet\')" class="typeIcon"><img class="typeImg" width="24" height="24" src="imgs/internet.png"/></div>');
           }
           if($type=="office"){
-            echo('<div class="typeIcon selected"><img class="typeImg" width="24" height="24" src="imgs/office.png"/><div class="typelbl">Office</div></div>');
+            echo('<div class="typeIcon selected"><img class="typeImg" width="24" height="24" src="imgs/office.png"/></div>');
           }else{
-            echo('<div onclick="loadType(\'office\')" class="typeIcon"><img class="typeImg" width="24" height="24" src="imgs/office.png"/><div class="typelbl">Office</div></div>');
+            echo('<div onclick="loadType(\'office\')" class="typeIcon"><img class="typeImg" width="24" height="24" src="imgs/office.png"/></div>');
           }
           if($type=="developper"){
-            echo('<div class="typeIcon selected"><img class="typeImg" width="24" height="24" src="imgs/developer.png"/><div class="typelbl">Devs</div></div>');
+            echo('<div class="typeIcon selected"><img class="typeImg" width="24" height="24" src="imgs/developer.png"/></div>');
           }else{
-            echo('<div onclick="loadType(\'developper\')" class="typeIcon"><img class="typeImg" width="24" height="24" src="imgs/developer.png"/><div class="typelbl">Devs</div></div>');
+            echo('<div onclick="loadType(\'developper\')" class="typeIcon"><img class="typeImg" width="24" height="24" src="imgs/developer.png"/></div>');
           }
+          echo('<input type="text" autofocus value="'.$query.'" placeholder="Search '.$type.'" onkeydown="search(this)" class="inputFind" />');
         ?>
       </div>
     <div class="mainPage">
@@ -62,7 +84,7 @@
           header('Location: index.php?type=all');
           exit;
         }
-        $json = json_decode(file_get_contents('http://zed-os.sourceforge.net/store/getAppList.php?type='.$_GET['type']),false);
+        $json = json_decode(file_get_contents('http://zed-os.sourceforge.net/store/getAppList.php?type='.$_GET['type'].'&query='.$_GET['query']),false);
         for ($i=0; $i < sizeof($json->files); $i++) { 
           echo('<div class="appCard" onClick="openApp(\''.$json->files[$i]->file.'\')">');
           echo('<img class="appIcon" src="http://zed-os.sourceforge.net/store/DIRECTORY/'.$json->files[$i]->file.'/favicon.png"/>');
