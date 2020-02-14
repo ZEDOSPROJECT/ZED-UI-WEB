b814bd3f4aeab73fcbf495c13940d7703e4e281d
Alexandre Nunes <alexandrenunes1331@gmail.com>
2020-02-14 00:44:40 +0000

    [FIX] Fix title tag

--- Frontend/src/OOBE/Screens/Welcome/Welcome.js
+++ Frontend/src/OOBE/Screens/Welcome/Welcome.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class Welcome extends React.Component {
     render(){
         return(<div>
-            <font size="20"><b>Welcome to ZED</b></font>
+            <h1>Welcome to ZED</h1>
             <p>Tank you for install ZED XP</p>
             <p>Let's speand a few minutes setting up your computer</p>
         </div>)
