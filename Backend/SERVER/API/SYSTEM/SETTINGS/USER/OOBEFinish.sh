37e255640c92c261f33980f39ee7c700e1c21edb
Alexandre Nunes <alexandrenunes1331@gmail.com>
2020-02-18 21:29:56 +0000

    [CHANGE] Change way copy user files

--- Backend/SERVER/API/SYSTEM/SETTINGS/USER/OOBEFinish.sh
+++ Backend/SERVER/API/SYSTEM/SETTINGS/USER/OOBEFinish.sh
@@ -6,6 +6,14 @@ password=$2
 useradd -m $username -p $password
 usermod -s /bin/bash $username
 echo -e "${password}\n${password}\n" | passwd $username
-cp -fr /home/zed /home/$username/
-cp -a /home/zed/.config/{.,}* /home/$username/.config/
+cp -a /home/zed/Desktop /home/$username/Desktop
+cp -a /home/zed/Documents /home/$username/Documents
+cp -a /home/zed/Downloads /home/$username/Downloads
+cp -a /home/zed/Music /home/$username/Music
+cp -a /home/zed/Pictures /home/$username/Pictures
+cp -a /home/zed/Public /home/$username/Public
+cp -a /home/zed/Templates /home/$username/Templates
+cp -a /home/zed/Videos /home/$username/Videos
+cp -a /home/zed/ZED-UI-WEB /home/$username/ZED-UI-WEB
+cp -a /home/zed/.config/openbox /home/$username/Documents/.config/openbox
 reboot
