0e6356d29940fd79e59e099e0006e1ecf3ad40b0
Alexandre Nunes <alexandrenunes1331@gmail.com>
2020-02-18 20:35:44 +0000

    [FIX] fix copying files

--- Backend/SERVER/API/SYSTEM/SETTINGS/USER/OOBEFinish.sh
+++ Backend/SERVER/API/SYSTEM/SETTINGS/USER/OOBEFinish.sh
@@ -6,5 +6,5 @@ password=$2
 useradd -m $username -p $password
 usermod -s /bin/bash $username
 echo -e "${password}\n${password}\n" | passwd $username
-cp -fr /home/zed/* /home/$username/
+cp -fr /home/zed /home/$username
 reboot
