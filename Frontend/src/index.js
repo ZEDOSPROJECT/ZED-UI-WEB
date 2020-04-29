4ba26c54006e2687d81bf4d04da61dd052c544d2
Alexandre Nunes <alexandrenunes1331@gmail.com>
2020-04-29 17:08:25 +0100

    [REMOVE] Remove temporarly OOBE

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -2,19 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import OOBE from './OOBE/OOBE';
+// import OOBE from './OOBE/OOBE';
 import registerServiceWorker from './registerServiceWorker';
-import { REST_URL } from './REST_URL';
+// import { REST_URL } from './REST_URL';
 
 
-fetch(REST_URL+'/API/SYSTEM/SETTINGS/USER/getPaths.php')
-    .then(response => response.json())
-    .then(json => {
-        if(json['username']==="zed"){
-            ReactDOM.render(<OOBE />, document.getElementById('root'));
-        }else{
-            ReactDOM.render(<App />, document.getElementById('root'));
-        }
-});
+
+// TEMPORARY OOBE
+// fetch(REST_URL+'/API/SYSTEM/SETTINGS/USER/getPaths.php')
+//     .then(response => response.json())
+//     .then(json => {
+//         if(json['username']==="zed"){
+//             ReactDOM.render(<OOBE />, document.getElementById('root'));
+//         }else{
+//             ReactDOM.render(<App />, document.getElementById('root'));
+//         }
+// });
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 registerServiceWorker();
