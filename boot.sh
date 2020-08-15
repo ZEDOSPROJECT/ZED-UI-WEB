ee8710cee1a0a89dbc74b813263d3b26b708d6cc
Alexandre Nunes <alexandrenunes1331@gmail.com>
2020-08-15 14:10:06 +0100

    [FIX] Fix when updating user settings

--- boot.sh
+++ boot.sh
@@ -14,7 +14,11 @@ fi
 
 if ping -q -c 1 -W 1 8.8.8.8 >/dev/null; then
    git stash
-   git pull
+   if ! git pull
+   then
+      git checkout Backend/SERVER/API/SYSTEM/SETTINGS/USER/SETTINGS.json
+      git pull
+   fi
    git stash pop
 fi
 cd Frontend
