6a188f8e0f38d37b05644fbb3af3d9c70e79497c
Alexandre Nunes <alexandrenunes1331@gmail.com>
2020-11-05 19:32:06 +0000

    [ADD] On boot check if running in live

--- boot.sh
+++ boot.sh
@@ -1,51 +1,55 @@
 #!/bin/bash
 
-cd Backend
-./startBackend.sh&
-cd ..
-if [ -e ".branch" ]
-then
-   if ping -q -c 1 -W 1 8.8.8.8 >/dev/null; then
-      git checkout "$(<.branch)"
-      rm .branch
-   fi
-fi
+isLive=$(which ubiquity)
 
-if ping -q -c 1 -W 1 8.8.8.8 >/dev/null; then
-   cd updateInstaller
-   npm start&
+if [ -z $isLive ]; then
+   # is installed
+   cd Backend
+   ./startBackend.sh&
    cd ..
-   git stash
-   if ! git pull
+   if [ -e ".branch" ]
    then
-      git checkout Backend/SERVER/API/SYSTEM/SETTINGS/USER/SETTINGS.json
-      git pull
+      if ping -q -c 1 -W 1 8.8.8.8 >/dev/null; then
+         git checkout "$(<.branch)"
+         rm .branch
+      fi
    fi
-   ./updateInstaller/postUpdate.sh
-   git stash pop
-   killall electron
+
+   if ping -q -c 1 -W 1 8.8.8.8 >/dev/null; then
+      cd updateInstaller
+      npm start&
+      cd ..
+      git stash
+      if ! git pull
+      then
+         git checkout Backend/SERVER/API/SYSTEM/SETTINGS/USER/SETTINGS.json
+         git pull
+      fi
+      ./updateInstaller/postUpdate.sh
+      git stash pop
+      killall electron
+   fi
+
+   Xaxis=$(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f1)
+   Yaxis=$(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f2)
+   wget https://picsum.photos/${Xaxis}/${Yaxis} -q -O ./Backend/SERVER/Wallpapers/Images/onlineImage.jpg
+
+   cd Frontend
+   npm install
+   npm run electron&
+   sleep 1
+   npm start&
+   cd Backend
+   cd SERVER
+   cd API
+   cd APPS
+   php updateApps.php
+   sleep 10m
+   while [ true ]
+   do
+      php updateApps.php
+      sleep 30m
+   done
+else
+   ubiquity
 fi
-
-Xaxis=$(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f1)
-
-Yaxis=$(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f2)
-
-wget https://picsum.photos/${Xaxis}/${Yaxis} -q -O ./Backend/SERVER/Wallpapers/Images/onlineImage.jpg
-
-
-cd Frontend
-npm install
-npm run electron&
-sleep 1
-npm start&
-cd Backend
-cd SERVER
-cd API
-cd APPS
-php updateApps.php
-sleep 10m
-while [ true ]
-do
-	php updateApps.php
-	sleep 30m
-done
