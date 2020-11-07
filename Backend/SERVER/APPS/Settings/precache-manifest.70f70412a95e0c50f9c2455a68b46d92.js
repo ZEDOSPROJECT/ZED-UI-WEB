0ab013f279d96dd6937d3052ecb5f6a8ff39091a
Alexandre Nunes <alexandrenunes1331@gmail.com>
2020-11-07 14:54:22 +0000

    [UPDATE] Update settings app

---
+++ Backend/SERVER/APPS/Settings/precache-manifest.70f70412a95e0c50f9c2455a68b46d92.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "2cf12ddf5811be26bdb4",
+    "url": "./static/css/main.47a2a4de.chunk.css"
+  },
+  {
+    "revision": "2cf12ddf5811be26bdb4",
+    "url": "./static/js/main.615ea1d4.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "050df22e93ad85d4d782",
+    "url": "./static/js/2.989ab914.chunk.js"
+  },
+  {
+    "revision": "5ce5548384a9856d52267969ea668acb",
+    "url": "./static/media/logo.5ce55483.png"
+  },
+  {
+    "revision": "c920ca081ab5ba624f0dc11dfa0e2154",
+    "url": "./static/media/online.c920ca08.png"
+  },
+  {
+    "revision": "b6292e331a13956d3e3b2503e2062845",
+    "url": "./static/media/PC.b6292e33.png"
+  },
+  {
+    "revision": "081bdbdb31f659b9dcaabbf04cc1c21c",
+    "url": "./static/media/internet.081bdbdb.png"
+  },
+  {
+    "revision": "2b99dc5c1a25563d40f362a1a4cfc88e",
+    "url": "./static/media/network.2b99dc5c.png"
+  },
+  {
+    "revision": "7d20725bd40f4f390c0e86de0d2471fc",
+    "url": "./static/media/0.7d20725b.png"
+  },
+  {
+    "revision": "eafac626c619a6c6f3a331488b2b777c",
+    "url": "./static/media/2.eafac626.png"
+  },
+  {
+    "revision": "7cf37a6c99483e41b81d0313f4f87b02",
+    "url": "./index.html"
+  }
+];
