b8baa4f6bd6c46b7458488ab48eb4b7086d1d36c
Alexandre Nunes <alexandrenunes1331@gmail.com>
2020-11-09 09:34:41 +0000

    [CHANGE] Chnage update script

--- boot.sh
+++ boot.sh
@@ -4,9 +4,6 @@ isLive=$(which ubiquity)
 
 if [ -z $isLive ]; then
    # is installed
-   cd Backend
-   ./startBackend.sh&
-   cd ..
    if [ -e ".branch" ]
    then
       if ping -q -c 1 -W 1 8.8.8.8 >/dev/null; then
@@ -19,17 +16,17 @@ if [ -z $isLive ]; then
       cd updateInstaller
       npm start&
       cd ..
-      git stash
       if ! git pull
       then
-         git update-index --assume-unchanged Backend/SERVER/API/SYSTEM/SETTINGS/USER/SETTINGS.json
+         mv Backend/SERVER/API/SYSTEM/SETTINGS/USER/SETTINGS.json /tmp/SETTINGS.json
+         git checkout .
          git pull
+         mv /tmp/SETTINGS.json Backend/SERVER/API/SYSTEM/SETTINGS/USER/SETTINGS.json
       fi
       cd Backend/SERVER/API/SYSTEM/SETTINGS/USER/
       php updateSettings.php
       cd ../../../../../../
       ./updateInstaller/postUpdate.sh
-      git stash pop
       killall electron
    fi
 
@@ -37,6 +34,9 @@ if [ -z $isLive ]; then
    Yaxis=$(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f2)
    wget https://picsum.photos/${Xaxis}/${Yaxis} -q -O ./Backend/SERVER/Wallpapers/Images/onlineImage.jpg
 
+   cd Backend
+   ./startBackend.sh&
+   cd ..
    cd Frontend
    npm install
    npm run electron&
